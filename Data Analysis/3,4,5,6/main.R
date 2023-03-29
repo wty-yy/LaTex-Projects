7edd5c6172933c1b5e6ac3710e6ef7b26a5bfbe9
wty <993660140@qq.com>
2023-03-29 09:59:33 +0800

    update

---
+++ Data Analysis/3,4,5,6/main.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/99366/Documents/GitHub/LaTex-Projects/Data Analysis/3,4,5,6")
+data <- read.table(file = "data.csv", sep = ",")
+X <- data[1:10, ]
+Y <- data[11:20, ]
+print(t.test(X, Y, var.equal = FALSE, paired = FALSE, mu = 0))
+# var.equal为FALSE表示假设两个总体方差不相等，paired为FALSE表示假设两个样本是独立的，mu为0表示检验两个总体均值是否相等
