53872f935d0c86c87accd9b48291e5399ea957cd
wty-yy <wty-yy@github.com>
2024-06-11 11:01:44 +0800

    update

--- undergraduate_program/coding/test.py
+++ undergraduate_program/coding/test.py
@@ -1,6 +1,7 @@
 def generate(maxn, filename):
   for i in range(1, maxn+1):
+    # print(r"\includegraphics[width=\textwidth, page="+str(i)+r", trim = 30mm 20mm 30mm 20mm, clip]{"+str(filename)+r"}")
     print(r"\includegraphics[width=\textwidth, page="+str(i)+r", trim = 15mm 20mm 15mm 20mm]{"+str(filename)+r"}")
   
-generate(10, "pdfs/外文翻译YOLOv4原文.pdf")
-generate(9, "pdfs/外文翻译YOLOv4译文.pdf")
+generate(16, "pdfs/师梓豪_2204112376_外文翻译原文.pdf")
+generate(16, "pdfs/师梓豪_2204112376_外文翻译译文.pdf")
